94ac4568c8e21b811323c2545e590912bc8fe23a
Kati Stark <kati.stark95@gmail.com>
2026-05-18 14:44:12 +0100

    Add Album data model to client

---
+++ Record-Shop-Frontend/Record-Shop-Frontend.Client/Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Record_Shop_Frontend.Client.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        [Required(ErrorMessage = "Name Is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Artist Is required")]
+        public string Artist { get; set; }
+        [Required(ErrorMessage = "ReleaseYear Is required")]
+        public string ReleaseYear { get; set; }
+        [Required(ErrorMessage = "Genre Is required")]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "Price Is required")]
+        public double? Price { get; set; }
+        [Required(ErrorMessage = "Stock Is required")]
+        public int? Stock { get; set; }
+    }
+}
