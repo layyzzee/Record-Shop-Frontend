b726547e0b2ce649f63264ef7dba310dda3de8cf
Kati Stark <kati.stark95@gmail.com>
2026-05-19 17:29:06 +0100

    Added connection to SQL Server for data

--- Record-Shop-Frontend/Record-Shop-Frontend.Client/Program.cs
+++ Record-Shop-Frontend/Record-Shop-Frontend.Client/Program.cs
@@ -7,6 +7,8 @@ namespace Record_Shop_Frontend.Client
         static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7121/") });
+
 
             await builder.Build().RunAsync();
         }
--- Record-Shop-Frontend/Record-Shop-Frontend/Program.cs
+++ Record-Shop-Frontend/Record-Shop-Frontend/Program.cs
@@ -14,6 +14,8 @@ namespace Record_Shop_Frontend
                 .AddInteractiveServerComponents()
                 .AddInteractiveWebAssemblyComponents();
 
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7121/") });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
